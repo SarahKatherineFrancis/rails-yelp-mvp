f209e42f8e256ccaebae3412e25ef5b4a212fc90
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-01 14:04:51 +0200

    added 5 restaurants using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+p "Clean DB"
+
+Restaurant.destroy_all
+
+p "populating DB"
+5.times do
+  res = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  rew = Review.create(
+    content: Faker::Restaurant.review,
+    restaurant_id: res.id
+  )
+  p "Restaurant id: #{res.id} was created with review #{rew.id}"
+
+end
